a425933dbd425ea0b68c006b5ee72a5227f09b7b
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2024-07-12 00:11:46 -0500

    🏷️ (export): export course: model

--- lib/domain/domain.dart
+++ lib/domain/domain.dart
@@ -3,3 +3,4 @@ export 'entities/user/user.dart';
 export 'repositories/auth_repository.dart';
 export 'entities/token/token.dart';
 export 'errors/auth_errors.dart';
+export 'entities/curso/curso_model.dart';
